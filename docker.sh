fde8ff1eb5f8cc8db19f72bed5c65850dbf96a9f
Pavel Grigoryev <pavel.grigoryev@middleware.co.nz>
2020-03-18 11:02:57 +1300

    initial commit

---
+++ docker.sh
@@ -0,0 +1,4 @@
+docker login
+docker build .
+docker tag 278a347053d9 taroball/taro1
+docker push taroball/taro1
---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var http = require('http')
+var os = require('os')
+var app = express();
+const port = process.env.PORT || "3000";
+const head='<html><body bgcolor="#222530" text="#22CC22"><style>body { font-family: Arial, Helvetica, sans-serif; font-size: x-large;}</style><h1>ХУЙ!拿</h1> '
+const butt="</body></html>"
+system_info= 'hostname: '+os.hostname()+" / os: "+os.type()
+
+server = http.createServer(app).listen(3000);
+var host = server.address().address;
+// var port = server.address().port;
+
+
+app.get('/', function (req, res) {
+  currentDate = new Date();
+  ua = req.get('user-agent')
+  ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  content = `${currentDate} <br> ${ip} - ${ua}<br> `
+  res.send(head+ content + system_info+butt);
+  console.log(`${currentDate} - ${ip} - ${ua}`)
+});
+console.log(system_info);
+console.log('Example app listening at http://%s:%s', host, port);
